f0d7a764bc2e3f483447d739be26761184ae1132
Mariana Albuquerque <marianaaa1@al.insper.edu.br>
2022-07-26 13:42:31 -0300

    transforma base

--- transforma_base.py
+++ transforma_base.py
@@ -1,3 +1,23 @@
+lista = [
+  {
+    'titulo': 'Qual o resultado da operação 57 + 32?',
+    'nivel': 'facil',
+    'opcoes': {'A': '-19', 'B': '85', 'C': '89', 'D': '99'},
+    'correta': 'C'
+  },
+  {
+    'titulo': 'Qual a capital do Brasil?',
+    'nivel': 'facil',
+    'opcoes': {'A': 'Brasília', 'B': 'Rio de janeiro', 'C': 'São Paulo', 'D': 'Osasco'},
+    'correta': 'A'
+  },
+  {
+    'titulo': 'Quem é considerada a primeira pessoa programadora do mundo?!',
+    'nivel': 'medio',
+    'opcoes': {'A': 'Marie Curie', 'B': 'Alan Turing', 'C': 'Ada Lovelace', 'D': 'Edsger Dijkstra'},
+    'correta': 'C'
+  }
+]
 def transforma_base(lista):
     if lista == []:
         return {}
@@ -20,4 +40,4 @@ def transforma_base(lista):
         del transformada['dificil']
     return transformada
 
-    
+print(transforma_base(lista))
