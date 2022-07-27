cdd72126826f2bb9493c477260a43ced1ea9ea69
Mariana Albuquerque <marianaaa1@al.insper.edu.br>
2022-07-26 22:01:26 -0300

    comecinho

---
+++ fortuna.py
@@ -0,0 +1,17 @@
+#importando bibliotecas necessarias:
+import  base_de_questoes
+import gera_ajuda_em_uma_questao
+import questao_para_texto
+import sorteia_questao_inedita
+import sorteia_uma_questao
+import transforma_base
+import valida_questao
+import valida_questoes
+
+#comecando o jogo
+premio = 0
+pulo = 3
+ajuda = 2
+
+print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!')
+nome = input('Qual seu nome?')
