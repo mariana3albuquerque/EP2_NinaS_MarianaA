888729cd48f510f0642b95a1b3102f2713421b36
Mariana Albuquerque <marianaaa1@al.insper.edu.br>
2022-07-28 09:55:06 -0300

    oi

---
+++ questoes_novas.py
@@ -0,0 +1,27 @@
+#mais 3 questoes de cada nivel
+medio1 = {'titulo': 'De quem é a famosa frase “Penso, logo existo”?',
+'nivel': 'medio',
+'opcoes': {'A': 'Platão', 
+'B': 'Galileu Galilei',
+'C': 'Descartes',
+'D': 'Sócrates'},
+'correta': 'C' }
+
+medio2 = {'titulo': 'Quais o menor e o maior país do mundo?',
+'nivel':'medio' ,
+'opcoes': {'A':'Vaticano e Rússia', 'B': 'Nauru e China', 'C':'Mônaco e Canadá' , 'D':'Malta e Estados Unidos' }, 
+'correta': 'A'}
+
+medio3 = {'titulo': 'oi',
+'nivel':'oi' ,
+'opcoes': {'A':'oi', 'B': 'oi', 'C':'oi' , 'D':'oi' }, 
+'correta': 'oi'}
+
+
+#   MODELO DE QUESTAO:
+
+{'titulo': 'oi',
+'nivel':'oi' ,
+'opcoes': {'A':'oi', 'B': 'oi', 'C':'oi' , 'D':'oi' }, 
+'correta': 'oi'}
+
