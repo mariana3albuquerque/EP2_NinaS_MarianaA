5b944a36b948107911cbd2ec68cb3416df687910
Nina <ninars@al.insper.edu.br>
2022-07-28 10:32:50 -0300

    commit nina

--- questao_para_texto.py
+++ questao_para_texto.py
@@ -1,6 +1,6 @@
 def questao_para_texto(questao_dicionario,id):
     r ='\n----------------------------------------\n'
-    r += 'QUESTAO {}\n'.format(id)
+    r += '\nQUESTAO {}\n'.format(id)
     titulo = questao_dicionario['titulo']
     r+='\n{}\n'.format(titulo)
     r+='\nRESPOSTAS:\n'
